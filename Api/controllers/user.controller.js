1b42d396f0d00cd86c2bbe2690e5dedbca51b86f
shivam kukshal <shivamkukshal001@gmail.com>
2024-04-11 16:31:38 +0530

    add delete user api route

--- Api/controllers/user.controller.js
+++ Api/controllers/user.controller.js
@@ -54,3 +54,15 @@ import User from '../models/user.model.js';
         next(error);
       }
   };
+
+  export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+      return next(errorHandler(403, 'You are not allowed to delete this user'));
+    }
+    try {
+      await User.findByIdAndDelete(req.params.userId);
+      res.status(200).json('User has been deleted');
+    } catch (error) {
+      next(error);
+    }
+  };
